4d8964aad12839e24448e345e9dc9ecc2df50056
Rishwanth M <rishwanth1121@gmail.com>
2025-07-29 14:20:16 +0530

    New Commits

--- social_platform/settings.py
+++ social_platform/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-^6exvy)$dngc32fu$hz%b144+9rdsgzp4)ce!op7ye7bmfy+n6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
